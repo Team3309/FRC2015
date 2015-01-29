aad5b4cdae857cb73a0f6ee5349df6bb7dc76f47
Matthew Krager <metoofarmer@yahoo.com>
2015-01-28 16:28:09 -0800

    Added Button Recognition to main Robot Class

--- src/org/usfirst/frc/team3309/robot/ArisTOTEle.java
+++ src/org/usfirst/frc/team3309/robot/ArisTOTEle.java
@@ -1,7 +1,12 @@
 
 package org.usfirst.frc.team3309.robot;
 
+import org.usfirt.frc.team3309.subsystems.Drive;
+import org.usfirt.frc.team3309.subsystems.Intake;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -11,33 +16,95 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class ArisTOTEle extends IterativeRobot {
-    /**
-     * This function is run when the robot is first started up and should be
-     * used for any initialization code.
-     */
-    public void robotInit() {
+	 XboxController driverController = new XboxController(1);
+	    XboxController operatorController = new XboxController(2);
 
-    }
+	    private boolean buttonPressedLastTime = false;
+	    private Scheduler scheduler;
+	    private Drive mDrive;
+	    private Intake mIntake;
+	    
+	    //The command that will begin running at the start of autonomous
+	    private Command autoCommand;
+	    
+	    private boolean constantChanger = true;
+	    //Runs when Robot is turned on
+	    public void robotInit() {
+	        if(constantChanger) {
+	            //constantChanger frame = new constantChanger();
+	        }
+	        scheduler = Scheduler.getInstance();
+	        //make drive
+	        mDrive = Drive.getInstance();
+	        mIntake = Claw.getInstance();
+	        //sets it so all information about the drive will be printed repeatidly during driving
+	        mDrive.setPrintingDriveInfo(false);
+	    }
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    public void autonomousPeriodic() {
+	    //When first put into disabled mode
+	    public void disabledInit() {
 
-    }
+	    }
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-    
-    }
+	    //Called repeatedly in disabled mode
+	    public void disabledPeriodic() {
+
+	    }
+
+	    //Init to Auto
+	    public void autonomousInit() {
+	        autoCommand = new AutoForwardAndTurn();
+	        autoCommand.start();
+	    }
+
+	    //This function is called periodically during autonomous
+	    public void autonomousPeriodic() {
+	        scheduler.run();
+	    }
+
+	    //Init to Tele
+	    public void teleopInit() {
+	        mDrive.resetGyro();
+	        //autoCommand.cancel();
+	    }
+
+	    //This function is called periodically during operator control
+	    public void teleopPeriodic() {
+	        scheduler.run();
+
+	        //gets all 4 axis from driver remote and depending on what drive the robot is in, the values will be used accordingly
+	        mDrive.drive(driverController.getLeftX(), driverController.getLeftY(), driverController.getRightX(), driverController.getRightY());
+
+	        //changes drive
+	        if (driverController.getLB()) {
+	            mDrive.setTankDrive();
+	        } else {
+	            mDrive.setHaloDrive();
+	        }
+	        
+	       
+	        //changes the solenoid on and off for driveshifter
+	        if (driverController.getA()) {
+	            mDrive.setLowGearOn();
+	        } else if (driverController.getB()) {
+	            mDrive.setHighGearOn();
+	            if (mDrive.isPrintingDriveInfo()) {
+	                System.out.println("High Gear Enabled");
+	            }
+	        }
+
+	        //checks if triggers are pressed in any way shape or form
+	        if (driverController.getRB()) {
+	            mIntake.runClawInward();
+	        } else if (driverController.getLB()) {
+	            mIntake.runClawOutward();
+	        } else {
+	            mIntake.stopClaw();
+	        }
+
+	        if (mDrive.isPrintingDriveInfo()) {
+	            System.out.println("------------------------\n");
+	        }
+	    }
     
 }
