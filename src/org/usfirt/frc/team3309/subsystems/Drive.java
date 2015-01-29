e690bdc50a4e5d8857ec7304f76e0f3c0cd00743
Matthew Krager <metoofarmer@yahoo.com>
2015-01-28 17:24:23 -0800

    Revised Init Code for 2015 DriveTrain

--- src/org/usfirt/frc/team3309/subsystems/Drive.java
+++ src/org/usfirt/frc/team3309/subsystems/Drive.java
@@ -1,28 +1,26 @@
 package org.usfirt.frc.team3309.subsystems;
 
+import org.usfirst.frc.team3309.robot.ModifiedGyro;
+import org.usfirst.frc.team3309.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.CounterBase;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.PIDCommand;
 
 public class Drive {
 	private boolean isPrintingDriveInfo = false;
 
     //all of the sensors and motor controllers
-    private Victor[] leftVictors = new Victor[3];
-    private Victor[] rightVictors = new Victor[3];
+    private Victor[] leftVictors = new Victor[2];
+    private Victor[] rightVictors = new Victor[2];
     private Victor strafeVictor1;
-    private Victor strafeVictor2;
 
     private Encoder leftEncoder;
     private Encoder rightEncoder;
     private ModifiedGyro gyro;
-    private Solenoid driveShifterHighGear;
-    private Solenoid driveShifterLowGear;
-
-    //all the constants for which drive is being used
-    private final int MODE_HALO_DRIVE = 0;
-    private final int MODE_TANK_DRIVE = 1;
-    private int driveMode = 0;
 
     //enable this is you ever want to go just forward
     private boolean straightPidEnabled = false;
@@ -37,9 +35,6 @@ public class Drive {
     private boolean gyroEnabled = true;
     //the max angular velocity (duh)
     private int MAX_ANGULAR_VELOCITY = 720;
-
-    //speed barrier for encoder, when encoder.getRate() exceeds this value, high gear automatically happens
-    private final double SPEED_BARRIER = 40;
 
     //Now for all the possible kp constants
     private double KP_NORMAL = .002;
@@ -56,7 +51,6 @@ public class Drive {
 
     private int strafeCounter = 0;
     
-    private PIDCommand strafePIDCommander = new strafePIDCommander();
 
     public static Drive getInstance() {
         if (instance == null) {
@@ -71,28 +65,21 @@ public class Drive {
         //initialize Victors in their arrays
         leftVictors[0] = new Victor(RobotMap.DRIVE_LEFT_1);
         leftVictors[1] = new Victor(RobotMap.DRIVE_LEFT_2);
-        leftVictors[2] = new Victor(RobotMap.DRIVE_LEFT_3);
         rightVictors[0] = new Victor(RobotMap.DRIVE_RIGHT_1);
         rightVictors[1] = new Victor(RobotMap.DRIVE_RIGHT_2);
-        rightVictors[2] = new Victor(RobotMap.DRIVE_RIGHT_3);
-
         strafeVictor1 = new Victor(RobotMap.DRIVE_STRAFE_1);
-        strafeVictor2 = new Victor(RobotMap.DRIVE_STRAFE_2);
-
-        driveShifterHighGear = new Solenoid(RobotMap.DRIVE_DRIVESHIFTER_RIGHT);
-        driveShifterLowGear = new Solenoid(RobotMap.DRIVE_DRIVESHIFTER_LEFT);
 
         //initialize Encoders
-        leftEncoder = new Encoder(RobotMap.DRIVE_ENCODER_LEFT_A, RobotMap.DRIVE_ENCODER_LEFT_B, true, CounterBase.EncodingType.k1X);
-        rightEncoder = new Encoder(RobotMap.DRIVE_ENCODER_RIGHT_A, RobotMap.DRIVE_ENCODER_RIGHT_B, false, CounterBase.EncodingType.k1X);
-        leftEncoder.start();
-        rightEncoder.start();
+        leftEncoder = new Encoder(RobotMap.DRIVE_ENCODER_LEFT, RobotMap.DRIVE_ENCODER_LEFT, true, CounterBase.EncodingType.k1X);
+        rightEncoder = new Encoder(RobotMap.DRIVE_ENCODER_RIGHT, RobotMap.DRIVE_ENCODER_RIGHT, false, CounterBase.EncodingType.k1X);
         //initialize gyro
         gyro = new ModifiedGyro(RobotMap.DRIVE_GYRO);
 
-        StraightPID straight = new StraightPID();
+        
+        //pid To be used Later
+        /*StraightPID straight = new StraightPID();
         straightPID = new PIDController(.001, 0, .02, straight, straight);
-        straightPID.disable();
+        straightPID.disable();*/
     }
 
     double pidRequestedValue;
