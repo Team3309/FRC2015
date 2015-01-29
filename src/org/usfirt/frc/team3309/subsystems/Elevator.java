9653af35b2631be25453bc5170b6b2b5ebce3023
Matthew Krager <metoofarmer@yahoo.com>
2015-01-28 18:50:44 -0800

    Added Singleton to Elevator

--- src/org/usfirt/frc/team3309/subsystems/Elevator.java
+++ src/org/usfirt/frc/team3309/subsystems/Elevator.java
@@ -2,4 +2,13 @@ package org.usfirt.frc.team3309.subsystems;
 
 public class Elevator {
 
+	private static Elevator instance;
+	
+	public static Elevator getInstance() {
+		if(instance == null) {
+			instance = new Elevator();
+		}
+		return instance;
+	}
+			
 }
