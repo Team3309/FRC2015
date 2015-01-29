44d85bdb83913a0f7968d23fbd74f22daee5cd0a
Matthew Krager <metoofarmer@yahoo.com>
2015-01-28 17:09:46 -0800

    Updated RobotMap For roboRio

--- src/org/usfirst/frc/team3309/robot/RobotMap.java
+++ src/org/usfirst/frc/team3309/robot/RobotMap.java
@@ -3,21 +3,16 @@ package org.usfirst.frc.team3309.robot;
 public class RobotMap {
 
     //all of these are ports, change them as the robot is build
-    public static final int DRIVE_LEFT_1 = 1;
-    public static final int DRIVE_LEFT_2 = 2;
-    public static final int DRIVE_LEFT_3 = 3;
-    public static final int DRIVE_RIGHT_1 = 4;
-    public static final int DRIVE_RIGHT_2 = 5;
-    public static final int DRIVE_RIGHT_3 = 6;
-    public static final int DRIVE_STRAFE_1 = 7;
-    public static final int DRIVE_STRAFE_2 = 8;
-    public static final int CLAW_RIGHTSIDE = 9;
-    public static final int CLAW_LEFTSIDE = 10;
+    public static final int DRIVE_LEFT_1 = 0;
+    public static final int DRIVE_LEFT_2 = 1;
+    public static final int DRIVE_RIGHT_1 = 2;
+    public static final int DRIVE_RIGHT_2 = 3;
+    public static final int DRIVE_STRAFE_1 = 4;
+    public static final int CLAW_RIGHTSIDE = 5;
+    public static final int CLAW_LEFTSIDE = 6;
 
-    public static final int DRIVE_ENCODER_LEFT_A = 1;
-    public static final int DRIVE_ENCODER_LEFT_B = 2;
-    public static final int DRIVE_ENCODER_RIGHT_A = 3;
-    public static final int DRIVE_ENCODER_RIGHT_B = 4;
+    public static final int DRIVE_ENCODER_LEFT = 0;
+    public static final int DRIVE_ENCODER_RIGHT =1;
 
     public static final int DRIVE_GYRO = 1;
 
