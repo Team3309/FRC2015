23bcd32b0322f5b10e7afcf5738078a9cba31fd6
Matthew Krager <metoofarmer@yahoo.com>
2015-02-10 17:00:16 -0800

    added controllers to controllers

--- src/org/usfirst/frc/team3309/robot/Controllers.java
+++ src/org/usfirst/frc/team3309/robot/Controllers.java
@@ -2,4 +2,20 @@ package org.usfirst.frc.team3309.robot;
 
 public class Controllers {
 
+	private static Controllers instance;
+	
+	//All Controllers being used
+	public static XboxController driverController = new XboxController(0);
+	public static XboxController OperatorController = new XboxController(1);
+
+	public static Controllers getInstance() {
+		if (instance == null) {
+			instance = new Controllers();
+		}
+		return instance;
+	}
+	
+	private Controllers() {
+		
+	}
 }
