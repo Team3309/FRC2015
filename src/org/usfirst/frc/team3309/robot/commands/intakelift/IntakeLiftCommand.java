7748c2dac6359a340701d63a285eecdff6501313
Matthew Krager <metoofarmer@yahoo.com>
2015-02-11 19:04:55 -0800

    implements IntakeLiftCommand

--- src/org/usfirst/frc/team3309/robot/commands/intakelift/IntakeLiftCommand.java
+++ src/org/usfirst/frc/team3309/robot/commands/intakelift/IntakeLiftCommand.java
@@ -1,22 +1,28 @@
 package org.usfirst.frc.team3309.robot.commands.intakelift;
 
+import org.usfirst.frc.team3309.robot.subsystems.IntakeLift;
+
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.command.Command;
 
-public class IntakeLiftCommand extends Command{
+public class IntakeLiftCommand extends Command implements PIDSource, PIDOutput{
 
+	private PIDController controller; 
+	private double KPCONSTANT = .01;
+	private IntakeLift mIntakeLift = IntakeLift.getInstance();
 	public IntakeLiftCommand() {
 		super();
 	}
 	
 	@Override
 	protected void initialize() {
-		// TODO Auto-generated method stub
-		
+		controller = new PIDController(KPCONSTANT, 0 , 0, this, this);
 	}
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
 		
 	}
 
@@ -36,6 +42,18 @@ public class IntakeLiftCommand extends Command{
 	protected void interrupted() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void pidWrite(double output) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double pidGet() {
+		// TODO Auto-generated method stub
+		return mIntakeLift.;
 	}
 	
 	
