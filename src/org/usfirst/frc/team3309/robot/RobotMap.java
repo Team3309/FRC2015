780316ff7fe99449d7b42001ee322fe310e2827b
Matthew Krager <metoofarmer@yahoo.com>
2015-02-11 17:28:32 -0800

    updated robotMap with intake encoders

--- src/org/usfirst/frc/team3309/robot/RobotMap.java
+++ src/org/usfirst/frc/team3309/robot/RobotMap.java
@@ -1,25 +1,33 @@
 package org.usfirst.frc.team3309.robot;
 
+import edu.wpi.first.wpilibj.CounterBase;
+
 public class RobotMap {
 
-    //all of these are ports, change them as the robot is build
-    public static final int DRIVE_LEFT_1 = 0;
-    public static final int DRIVE_LEFT_2 = 1;
-    public static final int DRIVE_RIGHT_1 = 2;
-    public static final int DRIVE_RIGHT_2 = 3;
-    public static final int DRIVE_STRAFE_1 = 4;
-    public static final int CLAW_RIGHTSIDE = 5;
-    public static final int CLAW_LEFTSIDE = 6;
-    public static final int INTAKE_LIFT_LEFT = 7;
-    public static final int INTAKE_LIFT_RIGHT = 8;
-    public static final int TOTE_LIFT = 9;
+	// all of these are ports, change them as the robot is build
+	public static final int DRIVE_LEFT_1 = 0;
+	public static final int DRIVE_LEFT_2 = 1;
+	public static final int DRIVE_RIGHT_1 = 2;
+	public static final int DRIVE_RIGHT_2 = 3;
+	public static final int DRIVE_STRAFE_1 = 4;
+	public static final int CLAW_RIGHTSIDE = 5;
+	public static final int CLAW_LEFTSIDE = 6;
+	public static final int INTAKE_LIFT_LEFT = 7;
+	public static final int INTAKE_LIFT_RIGHT = 8;
+	public static final int TOTE_LIFT = 9;
 
-    public static final int DRIVE_ENCODER_LEFT_A = 0;
-    public static final int DRIVE_ENCODER_LEFT_B = 1;
-    public static final int DRIVE_ENCODER_RIGHT_A =2;
-    public static final int DRIVE_ENCODER_RIGHT_B = 3;
+	// ENCODERS
+	public static final int DRIVE_ENCODER_LEFT_A = 0;
+	public static final int DRIVE_ENCODER_LEFT_B = 1;
+	public static final int DRIVE_ENCODER_RIGHT_A = 2;
+	public static final int DRIVE_ENCODER_RIGHT_B = 3;
 
-    public static final int DRIVE_GYRO = 0;
-    
-    public static final int INTAKE_SOLENOID = 0;
+	public static final int INTAKE_LIFT_LEFT_ENCODER_A = 4;
+	public static final int INTAKE_LIFT_LEFT_ENCODER_B = 5;
+	public static final int INTAKE_LIFT_RIGHT_ENCODER_A = 6;
+	public static final int INTAKE_LIFT_RIGHT_ENCODER_B = 7;
+
+	public static final int DRIVE_GYRO = 0;
+
+	public static final int INTAKE_SOLENOID = 0;
 }
