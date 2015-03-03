2ea98a1953405a635a7eacbabaa2f8c11d533adb
Matthew Krager <metoofarmer@yahoo.com>
2015-03-02 18:39:06 -0800

    methods

--- src/org/usfirst/frc/team3309/robot/commands/intake/IntakeRunTime.java
+++ src/org/usfirst/frc/team3309/robot/commands/intake/IntakeRunTime.java
@@ -1,5 +1,37 @@
 package org.usfirst.frc.team3309.robot.commands.intake;
 
-public class IntakeRunTime {
+import edu.wpi.first.wpilibj.command.Command;
+
+public class IntakeRunTime extends Command{
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
