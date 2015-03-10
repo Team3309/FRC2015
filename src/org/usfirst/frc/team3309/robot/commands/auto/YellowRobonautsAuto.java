531604a54763c05b70d6b0e219bfd3495340627d
Matthew Krager <metoofarmer@yahoo.com>
2015-03-09 22:08:50 -0700

    3/9/15 Part 2
    Forgot to save 1 file

--- src/org/usfirst/frc/team3309/robot/commands/auto/YellowRobonautsAuto.java
+++ src/org/usfirst/frc/team3309/robot/commands/auto/YellowRobonautsAuto.java
@@ -1,5 +1,20 @@
 package org.usfirst.frc.team3309.robot.commands.auto;
 
-public class YellowRobonautsAuto {
+import org.usfirst.frc.team3309.robot.commands.drive.DriveForwardEncoderCounts;
+import org.usfirst.frc.team3309.robot.commands.intake.IntakeRunTime;
 
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class YellowRobonautsAuto extends CommandGroup{
+	
+	public YellowRobonautsAuto() {
+		this.addSequential(new DriveForwardEncoderCounts(300), 1);
+		this.addParallel(new IntakeRunTime(1, 0, 1), 1);
+		
+		this.addSequential(new DriveForwardEncoderCounts(-300));
+		this.addParallel(new IntakeRunTime(1, 0, 1), 1);
+		
+		
+	}
+	
 }
