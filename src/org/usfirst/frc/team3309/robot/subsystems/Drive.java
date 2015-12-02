c9992086e4ff4f2cea0fe5068cf8438bfacb36f5
Matthew Krager <metoofarmer@yahoo.com>
2015-12-02 15:24:40 -0800

    chezy

--- src/org/usfirst/frc/team3309/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team3309/robot/subsystems/Drive.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team3309.robot.subsystems;
 
+import org.usfirst.frc.team3309.driverstation.Controllers;
 import org.usfirst.frc.team3309.robot.ModifiedGyro;
 import org.usfirst.frc.team3309.robot.RobotMap;
 
@@ -13,8 +14,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 @SuppressWarnings("unused")
 public class Drive {
 	private boolean isPrintingDriveInfo = false;
-	
-	//test
+
+	// test
 
 	// all of the sensors and motor controllers
 
@@ -118,7 +119,7 @@ public class Drive {
 	}
 
 	private void driveHalo(double throttle, double turn, double strafe) {
-		
+
 		updateConstants();
 		double modifiedTurn;
 		double gyroKP = KP_NORMAL;
@@ -176,7 +177,7 @@ public class Drive {
 						pid_Kp_NoThrottle = pid_Kp_NoThrottle_Left;
 					// calculate drive
 					pidDrive = ((pid_Kp_NoThrottle * pidError));
-					
+
 					setLeft(-pidDrive);
 					setRight(pidDrive);
 				} else { // else the user is driving with strafe and throttle
@@ -238,6 +239,125 @@ public class Drive {
 		}
 
 	}
+	
+	 public double handleDeadband(double val, double deadband) {
+	        return (Math.abs(val) > Math.abs(deadband)) ? val : 0.0;
+	    }
+
+    double oldWheel, quickStopAccumulator;
+    private double throttleDeadband = 0.02;
+    private double wheelDeadband = 0.02;
+    
+    public static double limit(double v, double limit) {
+        return (Math.abs(v) < limit) ? v : limit * (v < 0 ? -1 : 1);
+    }
+	public void cheesyDrive(double throttle, double wheel, boolean isQuickTurn, boolean isHighGear) {
+
+
+		double wheelNonLinearity;
+
+		wheel = handleDeadband(wheel, wheelDeadband);
+		throttle = handleDeadband(throttle, throttleDeadband);
+
+		double negInertia = wheel - oldWheel;
+		oldWheel = wheel;
+
+		if (isHighGear) {
+			wheelNonLinearity = 0.6;
+			// Apply a sin function that's scaled to make it feel better.
+			wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel) / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+			wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel) / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+		} else {
+			wheelNonLinearity = 0.5;
+			// Apply a sin function that's scaled to make it feel better.
+			wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel) / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+			wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel) / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+			wheel = Math.sin(Math.PI / 2.0 * wheelNonLinearity * wheel) / Math.sin(Math.PI / 2.0 * wheelNonLinearity);
+		}
+
+		double leftPwm, rightPwm, overPower;
+		double sensitivity;
+
+		double angularPower;
+		double linearPower;
+
+		// Negative inertia!
+		double negInertiaAccumulator = 0.0;
+		double negInertiaScalar;
+		if (isHighGear) {
+			negInertiaScalar = 4.0;
+			sensitivity = .75;
+		} else {
+			if (wheel * negInertia > 0) {
+				negInertiaScalar = 2.5;
+			} else {
+				if (Math.abs(wheel) > 0.65) {
+					negInertiaScalar = 5.0;
+				} else {
+					negInertiaScalar = 3.0;
+				}
+			}
+			sensitivity = .85; // Constants.sensitivityLow.getDouble();
+		}
+		double negInertiaPower = negInertia * negInertiaScalar;
+		negInertiaAccumulator += negInertiaPower;
+
+		wheel = wheel + negInertiaAccumulator;
+		if (negInertiaAccumulator > 1) {
+			negInertiaAccumulator -= 1;
+		} else if (negInertiaAccumulator < -1) {
+			negInertiaAccumulator += 1;
+		} else {
+			negInertiaAccumulator = 0;
+		}
+		linearPower = throttle;
+
+		// Quickturn!
+		if (isQuickTurn) {
+			if (Math.abs(linearPower) < 0.2) {
+				double alpha = 0.1;
+				quickStopAccumulator = (1 - alpha) * quickStopAccumulator + alpha * limit(wheel, 1.0) * 5;
+			}
+			overPower = 1.0;
+			if (isHighGear) {
+				sensitivity = .8;
+			} else {
+				sensitivity = .8;
+			}
+			angularPower = wheel * sensitivity;
+		} else {
+			overPower = 0.0;
+			angularPower = Math.abs(throttle) * wheel * sensitivity - quickStopAccumulator;
+			if (quickStopAccumulator > 1) {
+				quickStopAccumulator -= 1;
+			} else if (quickStopAccumulator < -1) {
+				quickStopAccumulator += 1;
+			} else {
+				quickStopAccumulator = 0.0;
+			}
+		}
+
+		rightPwm = leftPwm = linearPower;
+		leftPwm += angularPower;
+		rightPwm -= angularPower;
+
+		if (leftPwm > 1.0) {
+			rightPwm -= overPower * (leftPwm - 1.0);
+			leftPwm = 1.0;
+		} else if (rightPwm > 1.0) {
+			leftPwm -= overPower * (rightPwm - 1.0);
+			rightPwm = 1.0;
+		} else if (leftPwm < -1.0) {
+			rightPwm += overPower * (-1.0 - leftPwm);
+			leftPwm = -1.0;
+		} else if (rightPwm < -1.0) {
+			leftPwm += overPower * (-1.0 - rightPwm);
+			rightPwm = -1.0;
+		}
+		this.setLeft(leftPwm);
+		this.setRight(rightPwm);
+
+	}
 
 	private void updateConstants() {
 		KP_NORMAL = SmartDashboard.getNumber("KP_DRIVE_CONSTANT", .003);
@@ -261,8 +381,8 @@ public class Drive {
 	public void drive(double leftX, double leftY, double rightX, double rightY) {
 		// reverse these because pushing the Y joysticks forward returns a
 		// negative value, this is fixed here
-
-		driveHalo(leftY, rightX, leftX);
+		this.cheesyDrive(leftY, rightX, Controllers.getInstance().driverController.getRB(), false);
+		//driveHalo(leftY, rightX, leftX);
 	}
 
 	public int getLeftEncoder() {
@@ -298,7 +418,7 @@ public class Drive {
 	}
 
 	public double getAverageCount() {
-		return (rightEncoder.get() );
+		return (rightEncoder.get());
 	}
 
 }
